f7c782c97f773576086f070a3c74912077a5995f
Ziyad Almohisen <ziyadalmohisen@ziyads-macbook-pro.local>
2019-04-22 10:35:22 +0300

    Initial Commit

---
+++ City-pixel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  City-pixel
+//
+//  Created by Ziyad almohisen on 22/04/2019.
+//  Copyright © 2019 Ziyad almohisen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
